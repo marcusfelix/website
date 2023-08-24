d02de8943743cc193f86a231acf359c5e23d0ee1
Marcus Felix <marcus.felix@gmail.com>
2023-08-24 15:39:28 -0300

    fix: readme.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), preact()],
+  output: "server",
+  adapter: cloudflare()
+});
